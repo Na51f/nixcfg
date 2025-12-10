bc631cb288c5908e97817235a69b5294bcdf91f8
Cephalode <git@cephalode.com>
2025-12-09 22:13:40 -0500

    terminal home manager

--- flake.nix
+++ flake.nix
@@ -41,8 +41,7 @@
           specialArgs = { inherit inputs outputs; };
           modules = [ 
 		    ./hosts/loligo 
-			niri.nixosModules.niri 
-			{
+			niri.nixosModules.niri {
 			  programs.niri.enable = true;
 			}
 		  ];
