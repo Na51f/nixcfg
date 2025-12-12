2f58ba9ccb02d7c2efa2d679756eeb4542793a7a
Cephalode <git@cephalode.com>
2025-12-12 15:04:07 -0500

    misc

---
+++ modules/nixos/hapalo/video.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, lib, ... }: {
+  services.xserver.videoDrivers = [
+    "modesetting"
+	"nvidia"
+  ];
+
+  hardware = {
+	nvidia = {
+  	  modesetting.enable = true;
+
+	  powerManagement = {
+	    enable = false;
+	    finegrained = true;
+	  };
+  
+	  open = false;
+  
+	  nvidiaSettings = true;
+	  package = config.boot.kernelPackages.nvidiaPackages.stable;
+  
+	  prime = {
+	    offload = {
+		  enable = true;
+		  enableOffloadCmd = true;
+	    };
+	    intelBusId = "PCI:0:2:0";
+	    nvidiaBusId = "PCI:1:0:0";
+	  };
+	};
+  };
+}
