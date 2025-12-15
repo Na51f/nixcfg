cb688d99209c1d00ec70f7ed97dd5a783af55502
Cephalode <git@cephalode.com>
2025-12-15 17:19:32 -0500

    added hapalo system

--- hosts/nixos/hapalo/default.nix
+++ hosts/nixos/hapalo/default.nix
@@ -1,14 +1,14 @@
 { config, pkgs, inputs, outputs, lib, ... }: {
   imports = [
-	./hardware-configuration.nix
+    ./hardware-configuration.nix
     ../.
-	../../../modules/nixos/hapalo
+    ../../../modules/nixos/hapalo
   ];
-  
+
   networking.hostName = "hapalo";
 
   environment.systemPackages = with pkgs; [
   ];
-  
+
   system.stateVersion = "25.05"; # Do not change
 }
