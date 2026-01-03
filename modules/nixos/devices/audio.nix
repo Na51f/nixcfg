3c594a99e21eb2464519bb4abe0d798ef74e3192
Cephalode <git@cephalode.com>
2026-01-02 22:29:21 -0500

    fix: 🐛 Microphone input fixed

--- modules/nixos/devices/audio.nix
+++ modules/nixos/devices/audio.nix
@@ -1,7 +1,12 @@
 {
+  pkgs,
   ...
 }:
 {
+  environment.systemPackages = with pkgs; [
+    pwvucontrol
+  ];
+
   security.rtkit.enable = true;
 
   services = {
@@ -11,7 +16,15 @@
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
-      wireplumber.enable = true;
+      wireplumber = {
+        enable = true;
+        extraConfig."90-fifine" = {
+          "node.rules" = [{
+          matches = [{ "node.name" = "alsa_input.usb-MV-SILICON_fifine_SC3_20190808-00.pro-input-0"; }];
+            actions.update-props = { "node.suspend-on-idle" = false; };
+          }];
+        };
+      };
     };
   };
 }
